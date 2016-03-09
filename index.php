2cb29be9ff4535c149a70d6d3bbdb9e28bf4e31a
kit <yhaidari@ocls.ca>
2016-03-09 14:28:17 -0500

    contacts added

--- index.php
+++ index.php
@@ -12,5 +12,8 @@ and open the template in the editor.
     <body>
 		<h1>Bread Products</h1>
 		<p>List of bread products</p>
+                
+                List
+    <span>Notes: stuff</span>
     </body>
 </html>
